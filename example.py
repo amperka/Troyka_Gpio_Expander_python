0468ec7368bc7d8744116ee2fe9ac1cdada09ef9
acosinwork <acosinwork@yandex.ru>
2017-09-07 11:41:23 +0300

    try digitalWrite

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ exp = gpioexp.gpioexp()
 while True:
     exp.digitalWrite(0, 1)
     sleep(0.1)
-    exp.digitalWritePort(0, 0)
+    exp.digitalWrite(0, 0)
     sleep(0.1)
     print(exp.analogRead(3))
